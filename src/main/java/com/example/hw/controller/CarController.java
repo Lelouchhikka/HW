0502db7610faedf84ffa10534599c10d6dee16ef
Lelouchhikka <erasyl.kelman@gmail.com>
2021-08-26 14:42:35 +0600

    done

--- src/main/java/com/example/hw/controller/CarController.java
+++ src/main/java/com/example/hw/controller/CarController.java
@@ -1,8 +1,9 @@
 package com.example.hw.controller;
 
-import com.example.hw.db.DatabaseManager;
 import com.example.hw.entity.Car;
+import com.example.hw.services.CarService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,26 +14,29 @@ import java.util.List;
 @Slf4j
 @Controller
 public class CarController{
-    @GetMapping(value = "/")
-    public String Index(Model model){
-        List<Car> cars= DatabaseManager.getCars();
+
+    public CarController(CarService service) {
+        this.service = service;
+    }
+
+    @Autowired
+    private CarService service;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<Car> cars= service.getAllCars();
         model.addAttribute("cars",cars);
         return "index";
     }
-
-    @GetMapping(value = "/about")
-    public String About(){
-        return "about";
-    }
-    @PostMapping(value = "/add")
+    @PostMapping("/add")
     public String add(@RequestParam(name = "model") String model,
-                          @RequestParam(name = "price") Integer price,
-                          @RequestParam(name = "brand") String brand,
-                          @RequestParam(name="engineVolume") Double engineVolume
+                      @RequestParam(name = "price") Integer price,
+                      @RequestParam(name = "brand") String brand,
+                      @RequestParam(name="engineVolume") Double engineVolume
     ){
-        Car car= new Car(null,model,brand,engineVolume,price);
-        DatabaseManager.addCar(car);
-        DatabaseManager.addCar(car);
+        service.addCar(new Car( null,model,brand,engineVolume,price));
+
+
         return "redirect:/";
     }
 }
--- src/main/java/com/example/hw/entity/Car.java
+++ src/main/java/com/example/hw/entity/Car.java
@@ -2,10 +2,21 @@ package com.example.hw.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-@AllArgsConstructor
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Car {
-    private Long Id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String model;
     private String brand;
     private Double engineVolume;
